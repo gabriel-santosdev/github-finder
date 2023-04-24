31cae5df87e2d82f440c0e89e42899e25530f7da
gabriel-santosdev <gabrieljorgesilvasantos@outlook.com>
2023-04-24 11:07:57 -0300

    proejto finalizado

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css"
+
+import {Outlet} from 'react-router-dom'
+
+function App() {
+  
+
+  return (
+    <div className={styles.app}>
+      <h1>Github Finder</h1>
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
